35514379200ed2113c265d8704cf439bc024ebc0
DennisKappert <DennisKappert@users.noreply.github.com>
2019-05-03 16:48:32 +0200

    ReserveringDAO by Dennis

---
+++ src/main/java/se/skillytaire/belastingdienst/ee/data/ReserveringDAO.java
@@ -0,0 +1,115 @@
+package se.skillytaire.belastingdienst.ee.data;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import se.skillytaire.belastingdienst.ee.entity.Reservering;
+
+public class ReserveringDAO implements DAO<Reservering> {
+
+   @Override
+   public void add(Reservering that) {
+      EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("stuga");
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      EntityTransaction unmanagedTx = entityManager.getTransaction();
+      try {
+         unmanagedTx.begin();
+         entityManager.persist(that);
+         unmanagedTx.commit();
+      } catch (RuntimeException e) {
+         if (unmanagedTx != null) {
+            unmanagedTx.rollback();
+         }
+         throw e;
+      }
+      entityManager.close();
+      entityManagerFactory.close();
+   }
+
+   @Override
+   public Optional<Reservering> findByOID(Integer OID) {
+      EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("stuga");
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      Reservering gevondenReservering =
+            entityManager.find(Reservering.class, OID);
+      Optional<Reservering> optioneleReservering =
+            Optional.ofNullable(gevondenReservering);
+      entityManager.close();
+      entityManagerFactory.close();
+      return optioneleReservering;
+   }
+
+   @Override
+   public void update(Reservering that) {
+      EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("stuga");
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      EntityTransaction unmanagedTx = entityManager.getTransaction();
+      Reservering updateReservering = that;
+      try {
+         unmanagedTx.begin();
+         entityManager.merge(updateReservering);
+         unmanagedTx.commit();
+      } catch (RuntimeException e) {
+         if (unmanagedTx != null) {
+            unmanagedTx.rollback();
+         }
+         throw e;
+      }
+
+   }
+
+   @Override
+   public void delete(Reservering that) {
+      EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("stuga");
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      EntityTransaction unmanagedTx = entityManager.getTransaction();
+      Reservering deleteReservering = that;
+      try {
+         unmanagedTx.begin();
+         entityManager.remove(deleteReservering);
+         if (deleteReservering == null) {
+            throw new IllegalArgumentException("Reservering bestaat niet!");
+         }
+         unmanagedTx.commit();
+      } catch (RuntimeException e) {
+         if (unmanagedTx != null) {
+            unmanagedTx.rollback();
+         }
+         throw e;
+      }
+   }
+
+   @Override
+   public void deleteByOID(Integer OID) {
+      EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("stuga");
+      EntityManager entityManager = entityManagerFactory.createEntityManager();
+      EntityTransaction unmanagedTx = entityManager.getTransaction();
+      Reservering deleteReserveringByOID;
+      try {
+         unmanagedTx.begin();
+         deleteReserveringByOID = entityManager.find(Reservering.class, OID);
+         entityManager.remove(deleteReserveringByOID);
+         if (deleteReserveringByOID == null) {
+            throw new IllegalArgumentException(
+                  "Reservering met opgegeven OID bestaat niet!");
+         }
+         unmanagedTx.commit();
+      } catch (RuntimeException e) {
+         if (unmanagedTx != null) {
+            unmanagedTx.rollback();
+         }
+         throw e;
+      }
+
+   }
+
+}
---
+++ src/test/java/se/skillytaire/belastingdienst/ee/data/ReserveringDAOTest.java
@@ -0,0 +1,22 @@
+package se.skillytaire.belastingdienst.ee.data;
+
+import org.junit.Test;
+
+import se.skillytaire.belastingdienst.ee.entity.Reservering;
+
+public class ReserveringDAOTest {
+   @Test
+   public void testDaoAdd() {
+      Reservering reservering = new Reservering();
+/*
+ * TODO: Nog afmaken
+ */
+//            //Klant.getBuilder().addEmail("test@test.com").addUsername("username").addPassword("password").build();
+//
+//      assertFalse(dao == null);
+//      assertFalse(reservering == null);
+//      dao.add(reservering);
+//      assertTrue("Klant is opgeslagen", reservering.isPersistant());
+
+   }
+}
