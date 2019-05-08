fd22d98c74830dc6edb00e245f164ec289e0a72b
Reinier van Leussen <rvanleussen@outlook.com>
2019-05-08 20:59:26 +0200

    Add sources and javadoc jars to install.cmd

--- installs/install.sh
+++ installs/install.sh
@@ -15,3 +15,5 @@ mvn install:install-file -Dfile=se.skillytaire.service.wheater.klart-1.0.1-sourc
 mvn install:install-file -Dfile=se.skillytaire.service.wheater.klart-1.0.1-javadoc.jar -DgroupId=se.skillytaire.service.wheater -DartifactId=klart -Dversion=1.0.0 -Dpackaging=jar -Dclassifier=javadoc
 
 mvn install:install-file -Dfile=Builder-0.9.8.jar -DpomFile=Builder-0.9.8.pom.xml
+mvn install:install-file -Dfile=Builder-0.9.8-sources.jar -DgroupId=com.rvlstudio -DartifactId=Builder -Dversion=0.9.8 -Dpackaging=jar -Dclassifier=sources
+mvn install:install-file -Dfile=Builder-0.9.8-javadoc.jar -DgroupId=com.rvlstudio -DartifactId=Builder -Dversion=0.9.8 -Dpackaging=jar -Dclassifier=javadoc
