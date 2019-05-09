eeaedafe4b0e2abb565a9ef6ff8de7ee57323924
kappert <kappert@localhost>
2019-05-09 15:37:30 +0200

    Kleine verandering Reservering (RollBack)

--- src/main/java/se/skillytaire/belastingdienst/ee/entity/Reservering.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/entity/Reservering.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
@@ -30,6 +31,7 @@ public class Reservering extends AbstractEntity<Reservering> {
    public static final String DELETE_BY_OID = "Reservering_DeleteByOid";
    @NotNull
    @Basic
+   @Column(unique = true)
    private Integer reserveringsNummer;
    @NotNull
    @Basic
