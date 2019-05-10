0456d98d742c632e4e7e5884473e354580ee35f4
kappert <kappert@localhost>
2019-05-10 15:22:31 +0200

    Reservering add Tocht functie

--- src/main/java/se/skillytaire/belastingdienst/ee/entity/Reservering.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/entity/Reservering.java
@@ -16,96 +16,106 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
-//TODO klant en tocht verwerken: add /remove
 @Entity
-@Table(uniqueConstraints = {
-      @UniqueConstraint(name = "UniqueReservering", columnNames = {
-            "ReserveringsNummer", "reserveringsDatum", "verloopDatum" }) })
-@NamedQueries({ @NamedQuery(name = Reservering.FIND_BY_RESNUMMER,
-      query = "select a from Reservering a where a.reserveringsNummer=:reserveringsNummer"),
-      @NamedQuery(name = Reservering.DELETE_BY_OID,
-            query = "delete from Reservering a where a.oid=:oid") })
+@Table(uniqueConstraints = { @UniqueConstraint(name = "UniqueReservering", columnNames = { "ReserveringsNummer",
+		"reserveringsDatum", "verloopDatum" }) })
+@NamedQueries({
+		@NamedQuery(name = Reservering.FIND_BY_RESNUMMER, query = "select a from Reservering a where a.reserveringsNummer=:reserveringsNummer"),
+		@NamedQuery(name = Reservering.DELETE_BY_OID, query = "delete from Reservering a where a.oid=:oid") })
 public class Reservering extends AbstractEntity<Reservering> {
-   private static final long serialVersionUID = 1L;
-   public static final String FIND_BY_RESNUMMER = "Reservering_FindByResnummer";
-   public static final String DELETE_BY_OID = "Reservering_DeleteByOid";
-   @NotNull
-   @Basic
-   @Column(unique = true)
-   private Integer reserveringsNummer;
-   @NotNull
-   @Basic
-   private LocalDateTime reserveringsDatum;
-   @NotNull
-   @Basic
-   private LocalDateTime verloopDatum;
-   @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-   private List<Tocht<?>> mijnTochten = new ArrayList<Tocht<?>>();
-   @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-   private Klant mijnKlant;
+	private static final long serialVersionUID = 1L;
+	public static final String FIND_BY_RESNUMMER = "Reservering_FindByResnummer";
+	public static final String DELETE_BY_OID = "Reservering_DeleteByOid";
+	@NotNull
+	@Basic
+	@Column(unique = true)
+	private Integer reserveringsNummer;
+	@NotNull
+	@Basic
+	private LocalDateTime reserveringsDatum;
+	@NotNull
+	@Basic
+	private LocalDateTime verloopDatum;
+	@OneToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	private List<Tocht<?>> mijnTochten = new ArrayList<Tocht<?>>();
+	@NotNull
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	private Klant mijnKlant;
 
-   /**
-    * Developers should not use the default constructor. Please use the same
-    * visibility modifier "protected" for overriding classes.
-    */
-   public Reservering() {
-   }
+	/**
+	 * Developers should not use the default constructor. Please use the same
+	 * visibility modifier "protected" for overriding classes.
+	 */
+	public Reservering() {
+	}
 
-   public Reservering(final Integer reserveringsNummer) {
-      if (reserveringsNummer == null) {
-         throw new IllegalArgumentException("Het reserveringsNummer is Null");
-      }
-      this.reserveringsNummer = reserveringsNummer;
-   }
+	public Reservering(final Integer reserveringsNummer, final Klant mijnKlant) {
+		if (reserveringsNummer == null) {
+			throw new IllegalArgumentException("Het reserveringsNummer is Null");
+		}
+		if (mijnKlant == null) {
+			throw new IllegalArgumentException("Klant is Null");
+		}
+		this.reserveringsNummer = reserveringsNummer;
+		this.mijnKlant = mijnKlant;
+	}
 
-   public Reservering(final Reservering reservering) {
-      super(reservering);
-      this.reserveringsNummer = reservering.getReserveringsNummer();
-      this.reserveringsDatum = reservering.getReserveringsDatum();
-      this.verloopDatum = reservering.getVerloopDatum();
-   }
+	public Reservering(final Reservering reservering) {
+		super(reservering);
+		this.reserveringsNummer = reservering.getReserveringsNummer();
+		this.reserveringsDatum = reservering.getReserveringsDatum();
+		this.verloopDatum = reservering.getVerloopDatum();
+		this.mijnKlant = reservering.getKlant();
+	}
 
-   @Override
-   public String toString() {
-      StringBuilder builder = new StringBuilder();
-      builder.append("Reservering [Reserveringsnummer= ");
-      builder.append(this.reserveringsNummer);
-      builder.append("Reserverings datum= ");
-      builder.append(this.reserveringsDatum);
-      builder.append("Verloop datum= ");
-      builder.append(this.verloopDatum);
-      builder.append("]");
-      return builder.toString();
-   }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Klant [Mijn Klant= ").append(this.mijnKlant).append("Reservering Reserveringsnummer= ")
+				.append(this.reserveringsNummer).append("Reserverings datum= ").append(this.reserveringsDatum)
+				.append("Verloop datum= ").append(this.verloopDatum).append("]");
+		return builder.toString();
+	}
 
-   @Override
-   public int compareTo(final Reservering that) {
-      return this.getReserveringsNummer()
-            .compareTo(that.getReserveringsNummer());
-   }
+	@Override
+	public int compareTo(final Reservering that) {
+		return this.getReserveringsNummer().compareTo(that.getReserveringsNummer());
+	}
 
-   @Override
-   public int hashCode() {
-      return this.reserveringsNummer.hashCode();
-   }
+	@Override
+	public int hashCode() {
+		return this.reserveringsNummer.hashCode();
+	}
 
-   public Integer getReserveringsNummer() {
-      return this.reserveringsNummer;
-   }
+	public Klant getKlant() {
+		return this.mijnKlant;
+	}
 
-   public LocalDateTime getReserveringsDatum() {
-      return this.reserveringsDatum;
-   }
+	public Integer getReserveringsNummer() {
+		return this.reserveringsNummer;
+	}
 
-   public void setReserveringsDatum(final LocalDateTime reserveringsDatum) {
-      this.reserveringsDatum = reserveringsDatum;
-   }
+	public LocalDateTime getReserveringsDatum() {
+		return this.reserveringsDatum;
+	}
 
-   public LocalDateTime getVerloopDatum() {
-      return this.verloopDatum;
-   }
+	public void setReserveringsDatum(final LocalDateTime reserveringsDatum) {
+		this.reserveringsDatum = reserveringsDatum;
+	}
 
-   public void setVerloopDatum(final LocalDateTime verloopDatum) {
-      this.verloopDatum = verloopDatum;
-   }
+	public LocalDateTime getVerloopDatum() {
+		return this.verloopDatum;
+	}
+
+	public void setVerloopDatum(final LocalDateTime verloopDatum) {
+		this.verloopDatum = verloopDatum;
+	}
+
+	public boolean add(final Tocht<?> e) {
+		return this.mijnTochten.add(e);
+	}
+
+	public boolean remove(final Tocht<?> e) {
+		return this.mijnTochten.remove(e);
+	}
 }
--- src/test/java/se/skillytaire/belastingdienst/ee/entity/ReserveringTestFactory.java
+++ src/test/java/se/skillytaire/belastingdienst/ee/entity/ReserveringTestFactory.java
@@ -1,32 +1,46 @@
 package se.skillytaire.belastingdienst.ee.entity;
 
 import se.skillytaire.course.tools.jlc.AbstractComparableTestObjectFactory;
+import se.skillytaire.course.tools.jlc.GreaterThen;
+import se.skillytaire.course.tools.jlc.LessThen;
 import se.skillytaire.course.tools.jlc.TestFactory;
+import se.skillytaire.course.tools.jlc.That;
+import se.skillytaire.course.tools.jlc.This;
 
 @TestFactory
 public class ReserveringTestFactory
       extends AbstractComparableTestObjectFactory<Reservering> {
+	
+@This
+private Klant thisKlant;
+@That
+private Klant thatKlant;
+@GreaterThen
+private Klant greaterThenKlant;
+@LessThen
+private Klant lessThenKlant;
+	
    @Override
    public Reservering createThat() {
       Integer reserveringsNummer = new Integer(123456789);
-      return new Reservering(reserveringsNummer);
+      return new Reservering(reserveringsNummer, thatKlant);
    }
 
    @Override
    public Reservering createThis() {
       Integer reserveringsNummer = new Integer(987654321);
-      return new Reservering(reserveringsNummer);
+      return new Reservering(reserveringsNummer, thisKlant);
    }
 
    @Override
    public Reservering createGreaterThen() {
       Integer reserveringsNummer = new Integer(999999999);
-      return new Reservering(reserveringsNummer);
+      return new Reservering(reserveringsNummer, greaterThenKlant);
    }
 
    @Override
    public Reservering createLessThen() {
       Integer reserveringsNummer = new Integer(111111111);
-      return new Reservering(reserveringsNummer);
+      return new Reservering(reserveringsNummer, lessThenKlant);
    }
 }
