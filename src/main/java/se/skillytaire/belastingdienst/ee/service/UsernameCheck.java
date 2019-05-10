f889d9cd9f8b7a951967c35991b782ff0962c157
kappert <kappert@localhost>
2019-05-10 16:24:50 +0200

    UsernameCheckTest

--- src/main/java/se/skillytaire/belastingdienst/ee/service/UsernameCheck.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/service/UsernameCheck.java
@@ -1,11 +1,26 @@
 package se.skillytaire.belastingdienst.ee.service;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import se.skillytaire.belastingdienst.ee.persistance.KlantDAO;
 
 @Stateless
 public class UsernameCheck implements UsernameCheckRemote {
+	
+	public UsernameCheck(KlantDAO dao) {
+		super();
+		this.dao = dao;
+	}
+	
+	public UsernameCheck() {
+		super();
+	}
+
+	@Inject
+	private KlantDAO dao;
 	@Override
 	public boolean isBeschikbaar(String username) {
-		return false;
+		return !dao.find(username).isPresent();
 	}
 }
