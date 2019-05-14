4d8e87acbbf1bfb0e69071aeea98d58f2aa5c6a1
Reinier van Leussen <rvanleussen@outlook.com>
2019-05-14 10:23:28 +0200

    Account 100% coverage

--- src/test/java/se/skillytaire/belastingdienst/ee/entity/AccountTest.java
+++ src/test/java/se/skillytaire/belastingdienst/ee/entity/AccountTest.java
@@ -1,12 +1,28 @@
 package se.skillytaire.belastingdienst.ee.entity;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import se.skillytaire.course.tools.jlc.JLC;
 import se.skillytaire.course.tools.jlc.JLCRunner;
+import se.skillytaire.course.tools.jlc.This;
 
 @RunWith(JLCRunner.class)
-@JLC(value = Account.class, asJUnit = false)
+@JLC(value = Account.class, asJUnit = true)
 public class AccountTest {
-	
+	@This Reservering reservering;
+	@This Account account;
+
+	@Test
+	public void testAddReservering() {
+		assertTrue(this.account.add(this.reservering));
+	}
+
+	@Test
+	public void testRemoveReservering() {
+		assertTrue(this.account.add(this.reservering));
+		assertTrue(this.account.remove(this.reservering));
+	}
 }
