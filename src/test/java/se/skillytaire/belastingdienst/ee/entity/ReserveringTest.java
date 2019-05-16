6711852e9a279e1b5579a36f2e5bb046951dfb79
kappert <kappert@localhost>
2019-05-16 14:50:14 +0200

    Reservering Entity 100% coverage :)

--- src/test/java/se/skillytaire/belastingdienst/ee/entity/ReserveringTest.java
+++ src/test/java/se/skillytaire/belastingdienst/ee/entity/ReserveringTest.java
@@ -1,9 +1,19 @@
 package se.skillytaire.belastingdienst.ee.entity;
 
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import se.skillytaire.belastingdienst.ee.persistance.ReserveringDAO;
+import se.skillytaire.belastingdienst.ee.persistance.jpa.ReserveringJpaDAO;
 import se.skillytaire.course.tools.jlc.JLC;
 import se.skillytaire.course.tools.jlc.JLCRunner;
 import se.skillytaire.course.tools.jlc.That;
@@ -17,23 +27,64 @@ public class ReserveringTest {
 	@That
 	private Account thatAccount;
 	@This
-	private MeerTocht thisRivierTocht;
+	private MeerTocht thisMeerTocht;
 	@That
 	private RivierTocht thatRivierTocht;
 
+	private EntityManagerFactory factory;
+	private EntityManager entityManager;
+	private ReserveringDAO beanUnderTest;
+
+	@Before
+	   public void before() {
+	      JLCRunner.init(this);
+	   }
+
+	   @Before
+	   public void initJPA() {
+	      this.factory = Persistence.createEntityManagerFactory("stuga");
+	      this.entityManager = this.factory.createEntityManager();
+	      ReserveringJpaDAO.getInstance().setEntityManager(this.entityManager);
+	      this.beanUnderTest = ReserveringJpaDAO.getInstance();
+	   }
+
+
+	   @After
+	   public void destroyJPA() {
+	      if (this.entityManager != null) {
+	         this.entityManager.close();
+	      }
+	      if (this.factory != null) {
+	         this.factory.close();
+	         while (this.factory.isOpen() && !Thread.interrupted()) {
+	            try {
+	               Thread.sleep(100);
+	            } catch (InterruptedException e) {
+	               Thread.currentThread().interrupt();
+	            }
+	         }
+	      }
+	   }
+
+		@Test
+		public void testGetReserveringsNummer() {
+			Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
+			this.beanUnderTest.add(reservering1);
+			Optional<Integer> result = reservering1.getReserveringsNummer();
+			Assert.assertFalse(result.equals(null));
+		}
+	
 	@Test
 	public void fullConstructorTest() {
-		Reservering volledigeReservering = new Reservering(thisAccount, thisRivierTocht);
-//		int actual = volledigeReservering.getReserveringsNummer();
+		Reservering volledigeReservering = new Reservering(thisAccount, thisMeerTocht);
 		Account actualAccount = volledigeReservering.getAccount();
-//		Assert.assertEquals(volledigeReservering.getOid(), actual);
 		Assert.assertEquals(this.thisAccount, actualAccount);
 	}
 
 	@Test
 	public void equalsGelijkTest() {
-		Reservering reservering1 = new Reservering(thisAccount, thisRivierTocht);
-		Reservering reservering2 = new Reservering(thisAccount, thisRivierTocht);
+		Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
+		Reservering reservering2 = new Reservering(thisAccount, thisMeerTocht);
 		reservering1.setReserveringsDatum(ReserveringTestFactory.TIJD);
 		reservering2.setReserveringsDatum(ReserveringTestFactory.TIJD);
 		Assert.assertTrue(reservering1.equals(reservering2));
@@ -41,20 +92,20 @@ public class ReserveringTest {
 
 	@Test
 	public void equalsGelijkeRefTest() {
-		Reservering reservering1 = new Reservering(thisAccount, thisRivierTocht);
+		Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
 		Assert.assertTrue(reservering1.equals(reservering1));
 	}
 
 	@Test
 	public void equalsNullTest() {
-		Reservering reservering1 = new Reservering(thisAccount, thisRivierTocht);
+		Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
 		Assert.assertFalse(reservering1.equals(null));
 	}
 
 	@Test
 	public void equalsOngelijkTest() {
-		Reservering reservering1 = new Reservering(thisAccount, thisRivierTocht);
-		Reservering reservering2 = new Reservering(thatAccount, thisRivierTocht);
+		Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
+		Reservering reservering2 = new Reservering(thatAccount, thisMeerTocht);
 		Reservering reservering3 = new Reservering(thatAccount, thatRivierTocht);
 		Assert.assertFalse(reservering1.equals(reservering2));
 		Assert.assertFalse(reservering1.equals(reservering3));
@@ -62,7 +113,7 @@ public class ReserveringTest {
 
 	@Test
 	public void testHash() {
-		Reservering reservering1 = new Reservering(thisAccount, thisRivierTocht);
+		Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
 		int notExpected = 604107971;
 		int actual = reservering1.hashCode();
 		Assert.assertNotEquals(notExpected, actual);
@@ -70,13 +121,13 @@ public class ReserveringTest {
 
 	@Test
 	public void testAddTocht() {
-		Reservering reservering1 = new Reservering(thisAccount, thisRivierTocht);
+		Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
 		Assert.assertTrue(reservering1.add(thatRivierTocht));
 	}
 
 	@Test
 	public void testRemoveTocht() {
-		Reservering reservering1 = new Reservering(thisAccount, thisRivierTocht);
+		Reservering reservering1 = new Reservering(thisAccount, thisMeerTocht);
 		reservering1.add(thatRivierTocht);
 		Assert.assertTrue(reservering1.remove(thatRivierTocht));
 	}
