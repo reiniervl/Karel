2468e7b0032792e7a648fc9b3fe09a931874aaa0
Reinier van Leussen <rvanleussen@outlook.com>
2019-05-20 22:52:52 +0200

    Change return type

--- src/main/java/se/skillytaire/belastingdienst/ee/service/rest/LoginAccount.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/service/rest/LoginAccount.java
@@ -1,10 +1,14 @@
 package se.skillytaire.belastingdienst.ee.service.rest;
 
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("account")
 public class LoginAccount {
@@ -15,11 +19,24 @@ public class LoginAccount {
 
 	@POST
 	@Path("inloggen")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String inloggen(@FormParam("username") String username, @FormParam("password") String password) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response inloggen(@FormParam("username") String username, @FormParam("password") String password) {
+		JsonBuilderFactory factory = Json.createBuilderFactory(null);
+		JsonObject obj = factory.createObjectBuilder()
+			.add("success", false)
+			.add("reason", "username could not be found")
+			.add("code", 0)
+			.add("username", username)
+			.add("password", password)
+			.build();
+		
+		Response response = Response.ok()
+			.entity(obj.toString())
+			.type(MediaType.APPLICATION_JSON_TYPE)
+			.build();
 		
 
-		return String.format("Username: %s\tPassword: %s", username, password);
+		return response;
 	}
 
 	
