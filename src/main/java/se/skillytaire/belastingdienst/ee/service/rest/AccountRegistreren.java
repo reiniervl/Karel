07991153e429d93541c75eeed65bb800d832c551
kappert <kappert@localhost>
2019-05-21 16:11:59 +0200

    AccountRegistreren service

---
+++ src/main/java/se/skillytaire/belastingdienst/ee/service/rest/AccountRegistreren.java
@@ -0,0 +1,51 @@
+package se.skillytaire.belastingdienst.ee.service.rest;
+
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import se.skillytaire.belastingdienst.ee.service.account.NieuweKlantResultTO;
+import se.skillytaire.belastingdienst.ee.service.account.NieuweKlantTO;
+import se.skillytaire.belastingdienst.ee.service.account.RegistreerNieuwAccount;
+import se.skillytaire.belastingdienst.ee.service.account.RegistreerNieuweKlant;
+import se.skillytaire.belastingdienst.ee.service.account.UsernameCheck;
+
+@Path("registreren")
+public class AccountRegistreren {
+
+	@Inject
+	private UsernameCheck usernameCheck;
+	@Inject
+	private RegistreerNieuweKlant nieuweKlant;
+	@Inject
+	private RegistreerNieuwAccount nieuwAccount;
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response Registreren(@FormParam("your_name") String username, @FormParam("password") String password,
+			@FormParam("your_email") String email) {
+		boolean isAangemaakt = false;
+		if (usernameCheck.isBeschikbaar(username)) {
+			NieuweKlantTO nieuweKlantTo = new NieuweKlantTO(username, password, email);
+			NieuweKlantResultTO result = nieuweKlant.doIt(nieuweKlantTo);
+			if (result.getResult().isPresent()) {
+				nieuwAccount.registreer(result.getResult().get());
+			}
+		}
+
+		JsonBuilderFactory factory = Json.createBuilderFactory(null);
+		JsonObject obj = factory.createObjectBuilder().add("success", isAangemaakt).add("username", username)
+				.add("password", password).add("email", email).build();
+
+		Response response = Response.ok().entity(obj.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
+
+		return response;
+	}
+}
