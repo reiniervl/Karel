d4347c25778ee12247cf2b2763be6ca4193c8b24
kappert <kappert@localhost>
2019-05-21 15:32:48 +0200

    Login rest service

--- src/main/java/se/skillytaire/belastingdienst/ee/service/rest/LoginAccount.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/service/rest/LoginAccount.java
@@ -1,5 +1,6 @@
 package se.skillytaire.belastingdienst.ee.service.rest;
 
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
@@ -10,22 +11,29 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import se.skillytaire.belastingdienst.ee.service.account.PasswordCheck;
+import se.skillytaire.belastingdienst.ee.service.account.UsernameCheck;
+
 @Path("account")
 public class LoginAccount {
-// username:String
-// password:String
-
-// return: text/plain
+	
+//	@Inject
+//	private UsernameCheck usernameCheck;
+//	@Inject
+//	private PasswordCheck passwordCheck;
 
 	@POST
 	@Path("inloggen")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response inloggen(@FormParam("username") String username, @FormParam("password") String password) {
+		boolean valide = false;
+//		if (!usernameCheck.isBeschikbaar(username)) {
+//			valide = passwordCheck.isValide(username, password);
+//		} 		
+		
 		JsonBuilderFactory factory = Json.createBuilderFactory(null);
 		JsonObject obj = factory.createObjectBuilder()
-			.add("success", false)
-			.add("reason", "username could not be found")
-			.add("code", 0)
+			.add("success", valide)
 			.add("username", username)
 			.add("password", password)
 			.build();
