407a84ce678fbc0885692ec3f547245c3cce3cee
Reinier van Leussen <rvanleussen@outlook.com>
2019-05-27 15:27:08 +0200

    RivierTochtDAO werkt

--- src/main/java/se/skillytaire/belastingdienst/ee/persistance/jpa/RivierTochtJpaDAO.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/persistance/jpa/RivierTochtJpaDAO.java
@@ -7,7 +7,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import se.skillytaire.belastingdienst.ee.entity.RivierTocht;
@@ -15,12 +14,11 @@ import se.skillytaire.belastingdienst.ee.persistance.RivierTochtDAO;
 
 @Default
 @ApplicationScoped
-public class RivierTochtJpaDAO implements RivierTochtDAO {
+public class RivierTochtJpaDAO extends AbstractJPADAO<RivierTocht> implements RivierTochtDAO {
 	private static final RivierTochtJpaDAO instance = new RivierTochtJpaDAO();
-	@PersistenceContext
-	private EntityManager em;
 
 	public RivierTochtJpaDAO() {
+		super(RivierTocht.class);
 	}
 
 	public void setEntityManager(final EntityManager entityManager) {
@@ -35,37 +33,6 @@ public class RivierTochtJpaDAO implements RivierTochtDAO {
 	@Deprecated
 	public static RivierTochtJpaDAO getInstance() {
 		return RivierTochtJpaDAO.instance;
-	}
-
-	@Override
-	public void add(final RivierTocht rivierTocht) {
-		this.em.persist(rivierTocht);
-	}
-
-	@Override
-	public Optional<RivierTocht> findByOID(final Integer OID) {
-		RivierTocht gevondenRivierTocht = this.em.find(RivierTocht.class, OID);
-		return Optional.ofNullable(gevondenRivierTocht);
-	}
-
-	@Override
-	public RivierTocht update(final RivierTocht rivierTocht) {
-		this.em.merge(rivierTocht);
-		return this.em.merge(rivierTocht);
-	}
-
-	@Override
-	public boolean delete(final RivierTocht rivierTocht) {
-		this.em.remove(rivierTocht);
-		return true;
-	}
-
-	@Override
-	public boolean deleteByOID(final Integer OID) {
-		Query namedQuery = this.em.createNamedQuery(RivierTocht.DELETE_BY_OID);
-		namedQuery.setParameter("oid", OID);
-		int result = namedQuery.executeUpdate();
-		return result != 0;
 	}
 
 	@Override
--- src/main/java/se/skillytaire/belastingdienst/ee/service/ejb/BeschikbareRivierTochtenEJB.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/service/ejb/BeschikbareRivierTochtenEJB.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
-import javax.inject.Singleton;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
 
 import se.skillytaire.belastingdienst.ee.entity.RivierTocht;
 import se.skillytaire.belastingdienst.ee.persistance.RivierTochtDAO;
@@ -13,14 +15,18 @@ import se.skillytaire.belastingdienst.ee.service.activiteit.BeschikbareRivierToc
 import se.skillytaire.belastingdienst.ee.service.activiteit.BeschikbareRivierTochtenTO;
 
 @Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class BeschikbareRivierTochtenEJB implements BeschikbareRivierTochten {
-	@Singleton
+	@Inject
 	RivierTochtDAO rivierTochtDAO;
 
 	@Override
+	@TransactionAttribute(TransactionAttributeType.SUPPORTS)
 	public BeschikbareRivierTochtenResultTO geefTochten(BeschikbareRivierTochtenTO beschikbareRivierTochtenTO) {
 		BeschikbareRivierTochtenResultTO result;
-		Optional<List<RivierTocht>> tochtOptional = rivierTochtDAO.findBeschikbareTochten();
+		Optional<List<RivierTocht>> tochtOptional;
+		if(rivierTochtDAO != null) tochtOptional = rivierTochtDAO.findBeschikbareTochten();
+		else tochtOptional = Optional.ofNullable(null);
 		
 		if(tochtOptional.isPresent()) {
 			result = new BeschikbareRivierTochtenResultTO(tochtOptional.get());
