0c66b79f34a413984e44f2a71c8b63d5ac8aec33
kurt <kurt@localhost>
2019-05-28 14:12:09 +0200

    account registreren

--- src/main/java/se/skillytaire/belastingdienst/ee/service/rest/AccountRegistreren.java
+++ src/main/java/se/skillytaire/belastingdienst/ee/service/rest/AccountRegistreren.java
@@ -1,14 +1,20 @@
 package se.skillytaire.belastingdienst.ee.service.rest;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -31,7 +37,8 @@ public class AccountRegistreren {
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response Registreren(@FormParam("your_name") String username, @FormParam("password") String password,
-			@FormParam("your_email") String email) {
+			@FormParam("your_email") String email, @Context HttpServletRequest request, 
+			@Context HttpServletResponse response) throws IOException, ServletException {  
 		boolean isAangemaakt = false;
 		if (usernameCheck.isBeschikbaar(username)) {
 			NieuweKlantTO nieuweKlantTo = new NieuweKlantTO(username, password, email);
@@ -41,6 +48,10 @@ public class AccountRegistreren {
 				if (result.getResult().isPresent()) {
 					nieuwAccount.registreer(result.getResult().get());
 					isAangemaakt = true;
+					request.getSession().setAttribute("username", username);
+					response.sendRedirect(request.getContextPath() + "/login.html");
+				} else {
+					response.sendRedirect(request.getContextPath() + "/registreer.html");
 				}
 			} catch(ConstraintViolationException e) {
 				e.getConstraintViolations();
@@ -52,8 +63,8 @@ public class AccountRegistreren {
 		JsonObject obj = factory.createObjectBuilder().add("success", isAangemaakt).add("username", username)
 				.add("password", password).add("email", email).build();
 
-		Response response = Response.ok().entity(obj.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
+		Response response2 = Response.ok().entity(obj.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
 
-		return response;
+		return response2;
 	}
 }
